4613949407a6d2cf42cbdff8d98f3147652d56a7
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2019-04-23 19:45:57 +0200

    Displaying loading state with useStaate.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import axios from 'axios';
 function App() {
   const [results, setResults] = useState([]);
   const [query,setQuery] = useState('react hooks');
+  const [loading,setLoading] = useState(false);
   const searchInputRef = useRef();
 
   const getResults = async () => {
+    setLoading(true);
     const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
     setResults(response.data.hits);
+    setLoading(false);
   };
 
   const handleSearch = event => {
@@ -38,11 +41,15 @@ function App() {
       <button type="submit">Search</button>
         <button type="button" onClick={handleClearSearch}>Clear</button>
       </form>
-      {results.map(result => (
-          <ul key={result.objectID}>
-          <li ><a href={result.url}>{result.title}</a></li>
-          </ul>
-      ))}
+      {
+        loading ? "Loading..." :
+            results.map(result => (
+                  <ul key={result.objectID}>
+                    <li ><a href={result.url}>{result.title}</a></li>
+                  </ul>
+              ))
+      }
+
     </>
   );
 }
